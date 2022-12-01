711233031da1140ef35636acc6041a957dbe3839
ZEPHYRUS-G14\Kyle Logdat <logdatkyle@gmail.com>
2022-12-01 16:44:43 +0800

    -Created Prefabs Folder     - added slot prefab -Modified Items to have 'IsStackable'     -apple     -coal     -sword -Updated Inventory Manager -Added Slot Class -Added UI elemets for Inventory System

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,27 +1,115 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 
 public class InventoryManager : MonoBehaviour
 {
-    public ItemClass itemToAdd;
-    public ItemClass itemToRemove;
+    [SerializeField] private GameObject slotHolder;
+    [SerializeField] private ItemClass itemToAdd;
+    [SerializeField] private ItemClass itemToRemove;
+    
 
-    public List<ItemClass> items = new List<ItemClass> ();
+    public List<SlotClass> items = new List<SlotClass> ();
+
+    new GameObject[] slots;
 
     public void Start()
     {
+        slots = new GameObject[slotHolder.transform.childCount];
+
+        for (int i = 0; i < slotHolder.transform.childCount; i++) 
+            slots[i] = slotHolder.transform.GetChild(i).gameObject;
+
+        RefreshUI();
         Add(itemToAdd);
         Remove(itemToRemove);
     }
-    public void Add(ItemClass item)
+
+    public void RefreshUI()
     {
-        items.Add(item);
+        for(int i = 0; i < slots.Length; i++)
+        {
+            try
+            {
+                slots[i].transform.GetChild(0).GetComponent<Image>().enabled = true;
+                slots[i].transform.GetChild(0).GetComponent<Image>().sprite = items[i].GetItem().itemIcon;
+                if (items[i].GetItem().isStackable)
+                    if (items[i].getQuantity() <= 1)
+                        slots[i].transform.GetChild(1).GetComponent<Text>().text = "";
+                    else
+                        slots[i].transform.GetChild(1).GetComponent<Text>().text = items[i].getQuantity() + "";
+                        
+                else
+                    slots[i].transform.GetChild(1).GetComponent<Text>().text = "";
+            }
+            catch
+            {
+                slots[i].transform.GetChild(0).GetComponent<Image>().sprite = null;
+                slots[i].transform.GetChild(0).GetComponent<Image>().enabled = false;
+                slots[i].transform.GetChild(1).GetComponent<Text>().text = "";
+
+            }
+        }
+    }
+    public bool Add(ItemClass item)
+    {
+        // items.Add(item);
+        SlotClass slot = Contains(item);
+        if (slot != null && slot.GetItem().isStackable)
+        slot.AddQuantity(1);
+        else
+        {
+            if (items.Count < slots.Length)
+                items.Add(new SlotClass(item, 1));
+            else
+                return false;
+        }
+        RefreshUI();
+        return true;
     }
 
-    public void Remove(ItemClass item)
+    public bool Remove(ItemClass item)
     {
-        items.Remove(item);
+        SlotClass temp = Contains(item);
+        if (temp != null)
+        {
+            if (temp.getQuantity() > 1)
+            temp.SubQuantity(1);
+            else
+            {
+                SlotClass slotToRemove = new SlotClass();
+                foreach (SlotClass slot in items)
+                {
+                    if (slot.GetItem() == item)
+                    {
+                        slotToRemove = slot;
+                        break;
+                    }
+                }
+
+                items.Remove(slotToRemove);
+            }
+        }
+        else
+        {
+            return false;
+        }
+
+        RefreshUI();
+        return true;
     }
+
+    public SlotClass Contains(ItemClass item)
+    {
+        foreach (SlotClass slot in items){
+            if(slot.GetItem()== item)
+                return slot;
+        }
+        return null;
+    }
+
 }
